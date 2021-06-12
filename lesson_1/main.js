dc5f594e0931782143e21f896a59c11fdb697aaa
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-06-12 16:35:59 +0700

    lesson 21 : kiểu dữ liệu

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,22 +1,71 @@
 /**
- * Toán tử logic
- * 1. && - And
- * 2. || - Or
- * 3. ! - Not
+ * Kiểu dữ liệu trong JavaScript
+ * 
+ * 1. Dữ liệu nguyên thủy - Primitive Data
+ * -Number
+ * -String
+ * -Boolean
+ * -Undefined
+ * -Null
+ * -Symbol
+ * 
+ * 2. Dữ liệu phức tạp - Complex Data
+ * -Function
+ * -Object
+ * 
  */
 
+// Number type
 var a = 1;
 var b = 2;
-var c = 3;
+var c = 1.5;
+console.log(typeof a);
 
-if (a > 0 && b > 0) {
-    console.log('Điều kiện đúng');
-}
+//String type
 
-if (a > 0 || b > 0 || c < 0) {
-    console.log('Điều kiện đúng');
-}
+var fullName = " Hiep 'Hoang";
+console.log(fullName);
 
-if (!(a > 0)) {
-    console.log('Điều kiện đúng');
+//Boolean
+var isSuccess = true;
+
+// Undefined type
+var age;
+console.log(age);
+// Null 
+var isNull = null; //nothing
+console.log(isNull);
+console.log(typeof isNull);
+
+// Symbol
+var id = Symbol('id');  //unique
+var id2 = Symbol('id');  //unique
+
+console.log(id === id2);
+console.log(id2);
+
+// Function
+
+var myFunction = function () {
+    alert('Hi. Xin chào các bạn');
 }
+// myFunction();
+
+// Object types
+var myObject = {
+    name: 'Hiep Hoang',
+    age: 18,
+    address: 'Hue',
+    myFunction: function () {
+
+    }
+};
+console.log('myObject ', myObject);
+
+var myArray = [
+    'JavaScript',
+    'PHP',
+    'Ruby'
+]
+console.log('myArray ', myArray);
+
