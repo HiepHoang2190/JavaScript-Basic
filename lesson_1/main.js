b99fcceb6ffbb0634c77d9866f81bd37ce8d8755
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-06-12 11:40:18 +0700

    lesson14: Toán tử số học

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,32 +1,17 @@
 /**
- *
-Giới thiệu về toán tử trong  JavaScript
-1. Toán tử số học - Arithmetic
-2. Toán tử gán - Assignment
-3. Toán tử so sánh - Comparison
-4. Toán tử logic - Logical
+ * Toán tử số học
++   --> Cộng
+-   --> Trừ
+*   --> Nhân
+**  --> Lũy thừa
+/   --> Chia
+%   --> Chia lấy số dư
+++  --> Tăng 1 giá trị số
+--  --> Giảm 1 giá trị số
  */
 
-// 1.
-var a = 1 * 2;
-console.log(a);
-
-// 2.
-var fullName = 'Hoang Ngoc Hung';
-
-// 3
 
 var a = 1;
 var b = 2;
-
-if (a > b) {
-    alert('Dung');
-}
-
-// 4
-
-var a = 1;
-var b = 2;
-if (a > 0 && b > 0) {
-    alert('a & b lớn hơn 0');
-}
+var c = a - b;
+console.log(c);
