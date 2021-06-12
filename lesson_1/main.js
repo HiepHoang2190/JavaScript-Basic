a2d1b9d4ad6afcf6cc484bb009e47da6e590b06c
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-06-12 16:09:27 +0700

    lesson 19: kiểu dữ liệu boolean

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,23 +1,10 @@
-
 /**
- * Toán tử so sánh
-
-Toán tử
-
-==     -->    Bằng
-!=     --?    Không bằng
->      -->    Lớn hơn
-<      -->    Nhỏ hơn
->=     -->    Lớn hơn hoặc bằng
-<=     -->    Nhỏ hơn hoặc băng
+ * Boolean
  */
-
 
 var a = 1;
 var b = 2;
 
-if (a == 1) {
-    console.log('Điều kiện đúng!');
-} else {
-    console.log('Điều kiện sai!');
-}
+var isSuccess = a > b;
+
+console.log(isSuccess);
