6daa59edf57322ed5591a761c4aa494aadeb24b5
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-06-12 16:40:19 +0700

    lesson23: Toán tử so sánh- phaanf2

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,71 +1,18 @@
 /**
- * Kiểu dữ liệu trong JavaScript
- * 
- * 1. Dữ liệu nguyên thủy - Primitive Data
- * -Number
- * -String
- * -Boolean
- * -Undefined
- * -Null
- * -Symbol
- * 
- * 2. Dữ liệu phức tạp - Complex Data
- * -Function
- * -Object
- * 
+ * TOán tử so sánh - P2
  */
 
-// Number type
+/**
+ *  ===
+ *  !==
+ */
+
 var a = 1;
-var b = 2;
-var c = 1.5;
-console.log(typeof a);
+var b = '1';
 
-//String type
+console.log(a == b);
+console.log(a === b);
+console.log(a != b);
+console.log(a !== b);
 
-var fullName = " Hiep 'Hoang";
-console.log(fullName);
-
-//Boolean
-var isSuccess = true;
-
-// Undefined type
-var age;
-console.log(age);
-// Null 
-var isNull = null; //nothing
-console.log(isNull);
-console.log(typeof isNull);
-
-// Symbol
-var id = Symbol('id');  //unique
-var id2 = Symbol('id');  //unique
-
-console.log(id === id2);
-console.log(id2);
-
-// Function
-
-var myFunction = function () {
-    alert('Hi. Xin chào các bạn');
-}
-// myFunction();
-
-// Object types
-var myObject = {
-    name: 'Hiep Hoang',
-    age: 18,
-    address: 'Hue',
-    myFunction: function () {
-
-    }
-};
-console.log('myObject ', myObject);
-
-var myArray = [
-    'JavaScript',
-    'PHP',
-    'Ruby'
-]
-console.log('myArray ', myArray);
 
