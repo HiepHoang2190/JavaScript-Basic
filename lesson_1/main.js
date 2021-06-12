8018cff127841fbe92bf3ba1e73b9fb8c7e91368
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-06-12 15:58:38 +0700

    lesson16: toán tử gán

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,31 +1,19 @@
-// Toán tử ++ --
-// Prefix & Postfix
 
-var a = 6;
+/**
+ * Toán tử gán
 
+Toán tử         Ví dụ           Tương đương
+=               x = y           x = y
++=              x += y          x = x + y
+-+              x -= y          x = x - y
+*=              x *= y          x = x * y
+/=              x /= y          x = x / y
+**=             x **= y         x = x ** y
 
-// Việc 1 : a++ : +1 cho a, a= a+1 => a=7;
-//Việc 2 : ++a : Trả về a sau khi được +1
-console.log(++a);
-
-
-var b = 6;
-//  Việc 1 : `b copy `, `b copy` = 6
-//  Việc 2 : +1 cho b, b = b+1 => b=7
-// Việc 3 : trả về `b copy`
-var output = b++;
-console.log('output: ', output);
-console.log('b: ', b);
+ */
 
 
-var number = 6;
-var output2 = number++ + --number;
+var a = 1;
 
-// 6 + 6
-console.log('output2: ', output2);
-
-
-var output3 = ++number * 2 - number-- * 2;
-
-// 7*2 - 7*2 
-console.log('output3: ', output3);
+a = a + 2;
+console.log(a)
