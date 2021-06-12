94e51eff2d6a9bdf7e41efc788acf24aa42f122a
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-06-12 15:51:56 +0700

    lesson15: Toán tử ++ -- với tiền tố và hậu tố

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,17 +1,31 @@
-/**
- * Toán tử số học
-+   --> Cộng
--   --> Trừ
-*   --> Nhân
-**  --> Lũy thừa
-/   --> Chia
-%   --> Chia lấy số dư
-++  --> Tăng 1 giá trị số
---  --> Giảm 1 giá trị số
- */
+// Toán tử ++ --
+// Prefix & Postfix
+
+var a = 6;
 
 
-var a = 1;
-var b = 2;
-var c = a - b;
-console.log(c);
+// Việc 1 : a++ : +1 cho a, a= a+1 => a=7;
+//Việc 2 : ++a : Trả về a sau khi được +1
+console.log(++a);
+
+
+var b = 6;
+//  Việc 1 : `b copy `, `b copy` = 6
+//  Việc 2 : +1 cho b, b = b+1 => b=7
+// Việc 3 : trả về `b copy`
+var output = b++;
+console.log('output: ', output);
+console.log('b: ', b);
+
+
+var number = 6;
+var output2 = number++ + --number;
+
+// 6 + 6
+console.log('output2: ', output2);
+
+
+var output3 = ++number * 2 - number-- * 2;
+
+// 7*2 - 7*2 
+console.log('output3: ', output3);
