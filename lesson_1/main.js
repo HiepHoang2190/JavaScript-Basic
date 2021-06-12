73269dc10d02bf72e59cb2c1a77346066c54426c
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-06-12 17:10:11 +0700

    lesson26: Chuỗi

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,61 +1,36 @@
-// Hiểu hơn về câu lệnh điều kiện và phép so sánh
-
-
-var a = 1;
-var b = 2;
-
-var result = a < b;
-console.log('result ', result);
-
-if (a < b) {
-    console.log('A < B');
-} else {
-    console.log('A > B');
-}
-
-
-var result2 = a < b && a < 0;
-console.log(result2);
-
-
 /**
- * 0
- * false
- * ''
- * null
- * Nan
- * undefined
+ * CHUỖI TRONG JAVASCRIPT
+ * 
+ * 1. Tạo chuỗi
+ *      - Các cách tạo chuỗi
+ *      - Nên dùng cách nào? Lý do ?
+ *      - Kiểm tra data taype
+ * 2. Một số case sử dụng backslash (\)
+ * 3. Xem độ dài chuỗi
+ * 4. Chú ý độ dài khi viết Code
+ * 5.Template string ES6
+ * 
  */
 
-var result3 = 'A' && 'B' && 'C';
-console.log(result3);
+// var fullName = new String('Hiep Hoang');
+// console.log(typeof fullName);
 
-var result4 = null && 'B' && 'C';
-console.log(result4);
+var fullName = 'Son Dang la \'Sieu nhan\'';
+console.log(fullName);
 
-var result5 = 'A' && 'B' && NaN && 'D';
-console.log(result5);
+console.log(fullName.length);
 
-if (result5) {
-    console.log('Điều kiện đúng');
-} else {
-    console.log('Điều kiện sai');
-}
+// Cách xuống dòng để code gọn
 
-var result6 = 'A' || 'B' || 'C' || 'D';
-console.log('result6', result6);
+var fullName2 = "Một số case sử dụng backslash "
+    + "1. Một số case sử dụng backslash"
+    + "2. Một số case sử dụng backslash"
+    + "3. Một số case sử dụng backslash"
+    ;
+console.log(fullName2);
 
-var result7 = null || 'B' || 'C' || 'D';
-console.log('result7', result7);
-
-var result8 = 'A' || 'B' || undefined || 'D';
-console.log('result8', result8);
-
-var result9 = null || false || undefined || 0;
-console.log(result9);
-
-if (result9) {
-    console.log('Điều kiện đúng');
-} else {
-    console.log('Điều kiện sai');
-}
+// Template string ES6
+var firstName = 'Son';
+var lastName = "Dang";
+// console.log(' Toi la: ' + firstName + ' ' + lastName);
+console.log(`Toi la: ${firstName} ${lastName}`);
