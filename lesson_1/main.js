810befa309db9156913c04ebaa467d9ffe751302
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-06-13 10:49:42 +0700

    lesson 34 : return trong function

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,41 +1,9 @@
-// Tham số hàm - JavaScript cơ bản
-
-/**
- * 1. Tham số?
- *      - Định nghĩa
- *      - Kiểu dữ liệu
- *      - Tính private
- *      - 1 tham số
- *      - Nhiều tham s
- * 
- * 2. Arguments?
- *      - Đối tượng arguments
- *      - Giới thiệu vòng for
- */
-
-// function writeLog(message, message2) {
-//     if (message) {
-//         console.log(message);
-//     }
-//     if (message2) {
-//         console.log(message2);
-//     }
-// }
-
-// writeLog('TEST', 'TEST2');
+// Return trong hàm - Javascript cơ bản
 
 
-// function writeLog2() {
-//     console.log(arguments);
-// }
-// writeLog2('TEST', 'TEST2');
-
-function writeLog3() {
-    var myString = '';
-    for (var param of arguments) {
-        myString += ` ${param} -`
-    }
-    console.log(myString);
+function cong(a, b) {
+    return a + b;
 }
-writeLog3('TEST', 'TEST2', 'log 2', 'log3');
 
+var result = cong(2, 8);
+console.log(result);
