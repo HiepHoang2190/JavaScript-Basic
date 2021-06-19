e2227e704653c2a3521e1935f7745c454a7517f9
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-06-19 15:44:10 +0700

    lesson 43: lệnh rẽ nhánh switch

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,10 +1,17 @@
-// Câu lệnh rẽ nhánh - If else
+// Câu lệnh rẽ nhánh - Switch
+
 
 var date = 2;
-if (date === 2) {
-    console.log('Hôm nay là thứ 2');
-} else if (date === 3) {
-    console.log('Hôm nay là thứ 3');
-} else if (date === 4) {
-    console.log('Hôm nay là thứ 4');
+
+switch (date) {
+    case 2:
+    case 3:
+    case 4:
+        console.log('Hôm nay là thứ 2, 3, 4');
+        break;
+    case 5:
+        console.log('Hôm nay là thứ 5');
+        break;
+    default:
+        console.log('Không biết');
 }
