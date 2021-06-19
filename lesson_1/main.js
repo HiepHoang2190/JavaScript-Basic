3e001f1a552c4e6e930bd993cac5a2f1af06bc8d
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-06-19 23:47:06 +0700

    lesson 59: array reduce() method

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -40,43 +40,112 @@ var courses = [
     },
 ];
 
-// // Biến lưu trữ
-// var totalCoin = 0;
-
-// //Lặp qua các phần tử
-// for (var course of courses) {
-//     // Thực hiện việc lưu trữ
-//     totalCoin += course.coin;
-// }
-
-// console.log(totalCoin);
-
 var i = 0;
 
-function coinHandler(accumulator, currentValue, currentIndex, originArray) {
+var totalCoin = courses.reduce(function (total, course) {
     i++;
-    var total = accumulator + currentValue.coin;
-    console.table({
-        'Lượt chạy: ': i,
-        'Biến tích trữ: ': accumulator,
-        'Giá khóa học: ': currentValue.coin,
-        'Tích trữ được: ': total
-    });
 
-    // console.log(accumulator);
-    // console.log(currentValue);
-
-    return total;
-}
-
-var totalCoin = courses.reduce(coinHandler, 0); //initial value
+    console.log(i, total, course);
+    return total + course.coin;
+}, 0); // initial value
 
 console.log(totalCoin);
 
 
-// => viết ngắn gọn hơn như ở dưới
 
-var totalCoin2 = courses.reduce(function (accumulator, currentValue) {
-    return accumulator + currentValue.coin;
-}, 0);
-console.log(totalCoin2);
+var courses2 = [
+    {
+        id: 1,
+        name: 'JavaScript',
+        coin: 100
+    },
+    {
+        id: 2,
+        name: 'HTML, CSS',
+        coin: 200
+    },
+    {
+        id: 3,
+        name: 'Ruby',
+        coin: 220
+    },
+    {
+        id: 4,
+        name: 'PHP',
+        coin: 480
+    },
+    {
+        id: 5,
+        name: 'ReactJS',
+        coin: 500
+    },
+];
+
+var numbers = [100, 200, 220, 480, 500];
+
+var totalCoin = numbers.reduce(function (total, number) {
+    return total + number;
+});
+
+console.log(totalCoin);
+
+
+// Flat - " Làm phẳng" mảng từ Depth array - "Mảng sâu"
+
+depthArray = [1, 2, [3, 4], 5, 6, [7, 8, 9]];
+
+
+var flatArray = depthArray.reduce(function (flatOutput, depthItem) {
+    return flatOutput.concat(depthItem);
+}, []);
+
+console.log(flatArray);
+
+//  Lấy ra các khóa học đưa vào 1 mảng lưới
+
+var topics = [
+    {
+        topic: "Front-end",
+        courses: [
+            {
+                id: 1,
+                title: "HTML, CSS"
+            },
+            {
+                id: 2,
+                title: "JavaScript"
+            }
+        ]
+    },
+    {
+        topic: "Back-end",
+        courses: [
+            {
+                id: 1,
+                title: "PHP"
+            },
+            {
+                id: 2,
+                title: "NodeJS"
+            }
+        ]
+    }
+];
+
+var newCourses = topics.reduce(function (courses, topic) {
+
+    return courses.concat(topic.courses);
+}, []);
+
+console.log(newCourses);
+
+var htmls = newCourses.map(function (course) {
+    return `
+    <div>
+    <h2> ${course.title}</h2>
+    <p>ID: ${course.id}</p>
+    </div>
+    `;
+});
+
+console.log(htmls.join(''));
