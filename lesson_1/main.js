6f36acd328e9ac3f1b83db6c319f756e83f2f01d
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-06-19 11:21:07 +0700

    lessson 41: đối tượng Date

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,35 +1,9 @@
+// Đối tương Date
 
-/**
- * 
- * Object prototype - Basic
- * 1.Prototype là gì ?
- * 2.Sử dụng khi nào
- */
+var date = new Date();
+var year = date.getFullYear();
+var month = date.getMonth() + 1;
+var day = date.getDate();
 
-
-function User(firstName, lastName, avatar) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.avatar = avatar;
-
-    this.getName = function () {
-        return `${this.firstName} ${this.lastName}`;
-    }
-}
-
-User.prototype.className = 'F8';
-User.prototype.getClassName = function () {
-    return this.className;
-}
-var author = new User('Son', 'Đặng', 'Avatar 1');
-var user = new User('Vũ', 'Nguyễn', 'Avatar 2');
-
-
-
-console.log(author);
-console.log(user);
-
-console.log(author.className);
-console.log(user.className);
-console.log(author.getClassName());
-console.log(user.getClassName());
+console.log(day);
+console.log(`${year}/${month}/${day}`);
