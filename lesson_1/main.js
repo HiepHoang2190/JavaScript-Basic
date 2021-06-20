7e89ecb9c4993f16a4872e552b145a4be489e12e
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-06-20 10:56:31 +0700

    lesson 66: my forEach() method

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,12 +1,38 @@
 
 
-var courses = new Array(10);
-courses.push('JavaScript', 'PHP');
 
-courses.length = 10;
 
-console.log(courses);
 
-for (var index in courses) {
-    console.log(courses[index]);
+// var courses = [
+//     'JavaScript',
+//     'PHP',
+//     'Ruby'
+// ];
+
+// courses.length = 1000;
+
+// courses.forEach(function (course, index, array) {
+//     console.log(course, index, array);
+// });
+
+
+// forEach2
+Array.prototype.forEach2 = function (callback) {
+    for (var index in this) {
+        // console.log(index, this.hasOwnProperty(index));
+        if (this.hasOwnProperty(index)) {
+            callback(this[index], index, this);
+        }
+    }
 }
+
+
+var courses2 = new Array(100);
+
+courses2.push('JavaScript', 'Ruby');
+
+console.log(courses2);
+
+courses2.forEach2(function (course, index, array) {
+    console.log(course, index, array);
+});
