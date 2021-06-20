e100b7eef823fdc66f89a55cd5e498a3b52bf02e
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-06-20 10:16:52 +0700

    lesson 65: empty elements of array

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,33 +1,12 @@
-//Callback - Phần 2?
 
-// 1. Là hàm
-// 2. Truyền qua đối số
-// 3. được gọi lại (trong hàm nhận đối số)
-Array.prototype.map2 = function (callback) {
-    var output = [];
-    var arrayLength = this.length;
-    for (var i = 0; i < arrayLength; i++) {
-        var result = callback(this[i], i);
-        console.log('result: ', result);
-        output.push(result);
-    }
-    return output;
+
+var courses = new Array(10);
+courses.push('JavaScript', 'PHP');
+
+courses.length = 10;
+
+console.log(courses);
+
+for (var index in courses) {
+    console.log(courses[index]);
 }
-var courses = [
-    'JavaScript',
-    'PHP',
-    'Ruby'
-];
-
-// var htmls = courses.map(function (course) {
-//     return `<h2>${course}</h2>`;
-// });
-// console.log(htmls.join(''));
-
-
-
-
-var htmls = courses.map2(function (course, index) {
-    return `<h2>${course}</h2>`;
-});
-console.log(htmls.join(''));
