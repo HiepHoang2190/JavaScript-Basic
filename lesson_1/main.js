76ae56214a769a580acfc470f22751e1a2fa9fa6
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-06-20 09:52:55 +0700

    lesson 54:Callback phần 2

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,20 +1,33 @@
-// Callback ?
-
-// Là hàm (function) được truyền qua đối số
-// Khi gọi hàm khác
+//Callback - Phần 2?
 
 // 1. Là hàm
-// 2. Được truyền qua đối số
-
-function myFunction(param) {
-    if (typeof param === 'function') {
-        param('Học lập trình');
+// 2. Truyền qua đối số
+// 3. được gọi lại (trong hàm nhận đối số)
+Array.prototype.map2 = function (callback) {
+    var output = [];
+    var arrayLength = this.length;
+    for (var i = 0; i < arrayLength; i++) {
+        var result = callback(this[i], i);
+        console.log('result: ', result);
+        output.push(result);
     }
-
+    return output;
 }
+var courses = [
+    'JavaScript',
+    'PHP',
+    'Ruby'
+];
 
-function myCallback(value) {
-    console.log('Value: ', value);
-}
+// var htmls = courses.map(function (course) {
+//     return `<h2>${course}</h2>`;
+// });
+// console.log(htmls.join(''));
 
-myFunction(myCallback);
+
+
+
+var htmls = courses.map2(function (course, index) {
+    return `<h2>${course}</h2>`;
+});
+console.log(htmls.join(''));
