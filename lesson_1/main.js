6aefc62c2323cdf8ebb3ae1825b75a7c14237729
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-06-23 22:15:44 +0700

    lesson 70-71: HTML DOM

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,46 +1,19 @@
 
-// console.log(result);
+
+// HTML DOM
+
+// Có 3 thành phần
+
+/**
+ * 1. Element
+ * 2. Attribute
+ * 3. Text
+ */
 
 
+// -------------------------------
 
 
-// every2: true/false
+//  Javascript: Browser | Server (NodeJS)
 
-Array.prototype.every2 = function (callback) {
-    var output = true;
-    for (var index in this) {
-        if (this.hasOwnProperty(index)) {
-            var result = callback(this[index], index, this);
-            if (!result) {
-                output = false;
-                break;
-            }
-        }
-    }
-    return output;
-}
-
-
-var courses = [
-    {
-        name: 'JavaScript',
-        coin: 680,
-        isFinish: true,
-    },
-    {
-        name: 'PHP',
-        coin: 860,
-        isFinish: true,
-    },
-    {
-        name: 'Ruby',
-        coin: 980,
-        isFinish: false,
-    }
-];
-
-var result = courses.every2(function (course, index) {
-    return course.isFinish;
-});
-
-console.log(result);
+//  Browwser : HTML -> DOM -> WEB API
