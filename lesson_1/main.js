b4e72e3b76cad4c947d9f423dddf2fcecfe700e8
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-06-24 20:58:28 +0700

    lesson 73: Get element methods

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -5,7 +5,7 @@
 // Có 3 thành phần
 
 /**
- * 1. Element
+ * 1. Element: ID , class, tag, CSS selector, HTML collection
  * 2. Attribute
  * 3. Text
  */
@@ -13,5 +13,23 @@
 
 
 
-console.log(document);
-document.write('HELLO GUYS!');
+var headingNode = document.getElementById('heading');
+console.log({
+    elemment: headingNode
+});
+
+var headingNodes = document.getElementsByClassName('heading');
+console.log(headingNodes);
+
+
+var headingNodes = document.getElementsByTagName('p');
+console.log(headingNodes);
+
+var headingH2 = document.querySelector('.box .heading-2:first-child');
+
+console.log(headingH2);
+
+console.log(document.forms);
+console.log(document.forms[0]);
+console.log(document.forms['form-1']);
+console.log(document.forms.testForm);
