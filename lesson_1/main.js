9abfc2c577df660b91fd7e0a6f6462b5cc5de0b2
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-06-24 21:47:04 +0700

    lesson 75: get element methods-3

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,26 +1,12 @@
 
 
-// HTML DOM
 
-// Có 3 thành phần
+var h1Element = document.getElementsByTagName('h1')[0];
+console.log(h1Element);
 
-/**
- * 1. Element: ID , class, tag, CSS selector, HTML collection
- * 2. Attribute
- * 3. Text
- */
+var headings = document.querySelectorAll('.heading');
+for (var i = 0; i < headings.length; i++) {
+    console.log(headings[i]);
+}
 
-
-
-
-var boxNode = document.querySelector('.box-1');
-
-// Công việc 1 : Sử dụng tới `boxNode`
-console.log(boxNode);
-
-// Công việc 2: Sử dụng tới các li elements là con của `.box-1`
-
-
-console.log(boxNode.querySelectorAll('li'));
-console.log(boxNode.getElementsByTagName('li'));
-console.log(boxNode.getElementsByTagName('p'));
+console.log(document.forms);
